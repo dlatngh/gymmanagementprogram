ffd1e37d839c714a018b5fd208aca5b7f6d63b3c
notsooho <sooho.lim4@gmail.com>
2022-11-06 16:57:22 -0500

    uis are done

--- src/main/java/com/example/sma3/GymManagerMain.java
+++ src/main/java/com/example/sma3/GymManagerMain.java
@@ -3,6 +3,7 @@ package com.example.sma3;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -13,6 +14,7 @@ public class GymManagerMain extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Gym Manager");
+        stage.getIcons().add(new Image("file:src/main/resources/images/Scarlet-Knight.png"));
         stage.setScene(scene);
         stage.show();
     }
