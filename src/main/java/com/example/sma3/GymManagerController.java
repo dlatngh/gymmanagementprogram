d7423a5c636b45018ed46815abe760da2876da7b
kdavid109 <kdavid109@gmail.com>
2022-11-06 18:08:41 -0500

    nothingmuch

--- src/main/java/com/example/sma3/GymManagerController.java
+++ src/main/java/com/example/sma3/GymManagerController.java
@@ -1,12 +1,34 @@
 package com.example.sma3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class GymManagerController {
-    @FXML
-    private Label welcomeText;
 
+    @FXML
+    private TextField firstName, lastName;
+    @FXML
+    private TextField dateOfBirth, gymLocation;
+
+
+    /**
+     * conversion from textfield to Date, Location objects, and String objects
+     */
+    Date dob;
+    Location gymLoc;
+    String fName,lName;
+
+
+    public void createMembership(ActionEvent e){
+        //try{
+            fName = firstName.getText();
+            lName = lastName.getText();
+            dob = new Date(dateOfBirth.getText());
+       // }
+
+    }
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Running Gym Manager...");
