9f7007bf35d196fdff964baaf33f3935fb0f1464
notsooho <sooho.lim4@gmail.com>
2022-11-06 17:23:55 -0500

    lol

--- src/main/java/com/example/sma3/GymManagerMain.java
+++ src/main/java/com/example/sma3/GymManagerMain.java
@@ -13,7 +13,7 @@ public class GymManagerMain extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GymManagerMain.class.getResource("GymManagerView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("Gym Manager");
+        stage.setTitle("Gym Manager"); // test
         stage.getIcons().add(new Image("file:src/main/resources/images/Scarlet-Knight.png"));
         stage.setScene(scene);
         stage.show();
