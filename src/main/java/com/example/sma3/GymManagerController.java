329277958dfad331c26e7d912687bc031d9d7b34
notsooho <sooho.lim4@gmail.com>
2022-11-06 19:01:19 -0500

    remove member

--- src/main/java/com/example/sma3/GymManagerController.java
+++ src/main/java/com/example/sma3/GymManagerController.java
@@ -2,15 +2,45 @@ package com.example.sma3;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.TextArea;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class GymManagerController {
 
     @FXML
-    private TextField firstName, lastName;
+    private TextField firstName, lastName, locationField;
     @FXML
-    private TextField dateOfBirth, gymLocation;
+    private DatePicker dobField;
+    @FXML
+    private TextArea removeOut;
+    @FXML
+    private Button removeMember;
+
+    MemberDatabase memberDatabase = new MemberDatabase();
+
+    @FXML
+    private void removeMemberButton(ActionEvent event) {
+        String fname = firstName.getText();
+        String lname = lastName.getText();
+        String dobString = dobField.getValue().format(DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        Date dob = new Date(dobString);
+        Member member = memberDatabase.findMember(fname, lname, dob);
+        if (member != null) {
+            if (memberDatabase.remove(member)) {
+                removeOut.setText(member.getName() + " removed.");
+                System.out.println();
+
+            }
+        } else {
+            removeOut.setText(fname + " " + lname + " is not in the database.");
+        }
+
+    }
 
 
     /**
@@ -18,17 +48,18 @@ public class GymManagerController {
      */
     Date dob;
     Location gymLoc;
-    String fName,lName;
+    String fName, lName;
 
 
-    public void createMembership(ActionEvent e){
+    public void createMembership(ActionEvent e) {
         //try{
-            fName = firstName.getText();
-            lName = lastName.getText();
-            dob = new Date(dateOfBirth.getText());
-       // }
+        fName = firstName.getText();
+        lName = lastName.getText();
+//        dob = new Date(dobField.getText());
+        // }
 
     }
+
     @FXML
     protected void onHelloButtonClick() {
     }
